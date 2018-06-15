32aa09cfdbc8bb298a8231cae94af4696f5c01c5
Loredana.Hozan <loredana_h95@yahoo.com>
2018-06-15 11:11:51 +0300

    6 change

--- src/main/java/com/pluralsight/BookDAO.java
+++ src/main/java/com/pluralsight/BookDAO.java
@@ -1,13 +1,10 @@
 package com.pluralsight;
 
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import java.util.ArrayList;
 
 public class BookDAO {
